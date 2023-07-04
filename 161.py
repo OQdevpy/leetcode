c205e393f154d7929eba1d3034bd211c87012b8f
OQdev <o.qosimov@tuit.uz>
2023-07-04 16:32:31 +0500

    161

---
+++ 161.py
@@ -0,0 +1,6 @@
+url = "https://leetcode.com/problems/excel-sheet-column-title/description/"
+columnNumber = int(input("Enter column number: "))
+
+characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+result = characters[columnNumber//26-1]+characters[columnNumber%26-1]
+print(result)
