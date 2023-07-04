68c9a4c21ce05d67ddc66a9a492c8cc1b42aa737
OQdev <o.qosimov@tuit.uz>
2023-07-04 16:34:50 +0500

    161

--- 161.py
+++ 161.py
@@ -2,5 +2,5 @@ url = "https://leetcode.com/problems/excel-sheet-column-title/description/"
 columnNumber = int(input("Enter column number: "))
 
 characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-result = characters[columnNumber//26-1]+characters[columnNumber%26-1]
+result = characters[columnNumber%26-1] if columnNumber<26 else characters[columnNumber//26-1]+characters[columnNumber%26-1]
 print(result)
